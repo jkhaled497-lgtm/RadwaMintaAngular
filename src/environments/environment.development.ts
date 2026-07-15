9e139f27c213c393e7ed300b4d4d85b17085d2ba
Khaled <jkhaled497@gmail.com>
2026-07-15 05:22:53 +0300

    save

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,5 @@
 export const environment = {
     production: false,
-   // apiUrl: 'http://localhost:5124', // Local API
     
     apiUrl: 'https://khaled131122-001-site1.ftempurl.com', // Local API
     FilesURL: 'https://khaled131122-001-site1.ftempurl.com/files/' // Local API
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
     production: true,
     apiUrl: 'https://khaled131122-001-site1.ftempurl.com', // Local API
-   // apiUrl: 'http://localhost:5124', // Local API
     
     FilesURL: 'https://khaled131122-001-site1.ftempurl.com/files/' // Local API
     
